436eec6d4ba97d7de5a7ee5a1d83f67e027c2891
Anthony Gilardi <acgilardi@gmail.com>
2016-03-02 07:16:00 -0500

    working firebase to mobile text selection

--- app/components/read/book.ts
+++ app/components/read/book.ts
@@ -1,4 +1,16 @@
-import {Component} from 'angular2/core'
+import {Component} from 'angular2/core';
+import {Observable} from 'rxjs/Observable';
+import { observableFirebaseObject, observableFirebaseArray } from '../../vendor/angular2-firebase/observableFirebase.ts';
+import {NgWhen} from '../../vendor/angular2-firebase/ngWhen.ts';
+
+//import * as Firebase from 'firebase';
+
+//import { FIREBASE_ROOT } from '../../../config';
+//const FIREBASE_ORDER_GROUPS = `${FIREBASE_ROOT}/order-groups`;
+//const FIREBASE_ORDERS = `${FIREBASE_ROOT}/orders`;
+//const FIREBASE_ROOT = 'https://yayday.firebaseio.com/book/a1';
+
+declare var Firebase: any;
 
 @Component({
     selector: 'book',
@@ -6,15 +18,29 @@ import {Component} from 'angular2/core'
     templateUrl: './components/read/book.html'
 })
 export class BookComponent {
-   // constructor (public name:string = 'N/A') {};
+    public bookText: string;
+    public selectedWordId: string;
+    private bookRef: Observable<any>
 
-    public words: Array = "This is the book text".split(' ');
+    //private bookRef:FireBase;// = new Firebase("https://yayday.firebaseio.com/book/a1");
+    //private bookRef = new Firebase(FIREBASE_ROOT);
+    //this.orderGroupsRef = new Firebase(FIREBASE_ORDER_GROUPS);
+    //this.ordersRef = new Firebase(FIREBASE_ORDERS);
+    private words: Array;
 
-    clickWord(e) : void {
-        let bookRef = new Firebase("https://yayday.firebaseio.com/book/a1");
+    constructor() {
+        this.bookText = "This is the book text";
+        this.words = this.bookText.split(' ');
+        this.bookRef = new Firebase("https://yayday.firebaseio.com/book/a1");
+        this.bookRef.set({
+            bookText: this.bookText
+        });
+    };
 
-        bookRef.set({
-            selectedWordId: e.target.id,
+    clickWord(e):void {
+        this.selectedWordId = e.target.id;
+        this.bookRef.update({
+            selectedWordId: this.selectedWordId,
             selectedWord: e.target.innerText.trim()
         });
 
--- app/components/read/view.ts
+++ app/components/read/view.ts
@@ -1,23 +1,57 @@
-import {Component} from 'angular2/core'
+import {Component, NgZone, LifecycleEvent} from 'angular2/core'
 import {BookComponent} from './book'
+import {Observable} from 'rxjs/Observable';
+import { observableFirebaseObject, observableFirebaseArray } from '../../vendor/angular2-firebase/observableFirebase';
+import {NgWhen} from '../../vendor/angular2-firebase/ngWhen';
+
+declare var Firebase: any;
 
 @Component({
     selector: 'view',
     //directives:  [BookComponent],
     //template: '<div />'
-    templateUrl: './components/read/view.html'
+    templateUrl: './components/read/view.html',
+
 })
 export class ViewComponent {
-    public selectedWord : String = '_';
+    public bookText: string;
+    public selectedWord : string;
+    public words: Array;
+    private bookRef: Observable<any>;
+    //private bookRef = new Firebase("https://yayday.firebaseio.com/");
+    private zone = this;
+
     constructor() {
-        let bookRef = new Firebase("https://yayday.firebaseio.com/");
+        this.bookText = "(No book selected)";
+
+        //this.bookRef = new Firebase("https://yayday.firebaseio.com/book/a1");
+        //console.log(this.bookRef);
+        //this.bookText = observableFirebaseObject(new Firebase("https://yayday.firebaseio.com/"));
 
 
+        //}
+        //this.bookRef.child("book/a1/bookText").on("value", function (snapshot) {
+        //   //this.zone.run(() => {
+        //        this.bookText = snapshot.val();
+        //        this.words = this.bookText.split(' ');
+        //        console.log('Book Selected: ' + this.bookText);
+        //    //});
+        //});
+        //
+        //
+        //this.bookRef.child("book/a1/selectedWord").on("value", function (snapshot) {
+        //    //this.zone.run(() => {
+        //        this.selectedWord = snapshot.val();
+        //        console.log(snapshot.val());
+        //    //});
+        //})
+    }
 
-        bookRef.child("book/a1/selectedWord").on("value", function (snapshot) {
-            //alert(snapshot.val());  // Alerts "San Francisco"
-            this.selectedWord = snapshot.val();
-        })
+    ngOnInit() {
+
+        this.bookText = observableFirebaseObject(new Firebase("https://yayday.firebaseio.com/book/a1/")
+            .child("bookText"));
+        console.log(this.bookText)
     }
 
     //public selectedWord : String = '_';
